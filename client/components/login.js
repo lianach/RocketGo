deeedc0bc9934009e0e2843afc03ebe3c3991243
lianach <liana.andreea97@yhaoo.com>
2020-03-01 12:08:45 -0500

    Set up the stack

---
+++ client/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = (props) => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      This is a login page!
+    </div>
+  )
+}
+export default Login
---
+++ server/db/db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/rocketGo`,
+  {
+    logging: false
+  }
+)
+module.exports = db
+
+// This is a global Mocha hook used for resource cleanup.
+// Otherwise, Mocha v4+ does not exit after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close())
+}
